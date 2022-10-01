94b99e539e6bc0ba11ca7c2f14a629e5c5c418ce
Aleksandr Shatokhin <al.shatokhin2010@yandex.ru>
2022-10-01 21:27:18 +0300

    add register a user in the database

--- Assets/Scripts/RegistrationScene/RegistrationWindow.cs
+++ Assets/Scripts/RegistrationScene/RegistrationWindow.cs
@@ -36,8 +36,43 @@ public class RegistrationWindow : MonoBehaviour
 
     public void OnClickButtonCreateAccount()
     {
+        if (VerifyInput())
+        {
+            StartCoroutine(Register());
+        }
+        else
+        {
+            Debug.Log("¬ведены неправильные значени€");
+        }
+
         ClearInputField();
-        this.gameObject.SetActive(false);
-        logInWindow.SetActive(true);
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", inputFieldLogin.text);
+        form.AddField("password", inputFieldPassword.text);
+
+        WWW www = new WWW("http://localhost/SQLConnect/Register.php", form);
+        yield return www;
+
+        if (www.text == "0")
+        {
+            Debug.Log("User created!");
+
+            this.gameObject.SetActive(false);
+            logInWindow.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("User created failed. Error #" + www.text);
+        }
+    }
+
+    private bool VerifyInput()
+    {
+        bool isInputCorrectText = inputFieldLogin.text.Length >= 4 && inputFieldPassword.text.Length >= 8 ? true : false;
+        return isInputCorrectText;
     }
 }
